bf51faf90364fe2be5e384a653c7fd1c2cdfd985
Mathieu <mathieu.collet@gmail.com>
2020-05-02 22:19:37 +0200

    add portfolio app

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'portfolio',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = BASE_DIR
+MEDIA_URL = '/media/'
